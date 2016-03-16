d118b1a9473f31be6962a6078cb2b1ac63182a3a
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2016-03-16 16:19:08 +0900

    Entity を移動

---
+++ Entity/OAuth2/RefreshToken.php
@@ -0,0 +1,223 @@
+<?php
+
+namespace Eccube\Entity\OAuth2;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * RefreshToken
+ */
+class RefreshToken extends \Eccube\Entity\AbstractEntity
+{
+    /**
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $refresh_token;
+
+    /**
+     * @var integer
+     */
+    private $client_id;
+
+    /**
+     * @var integer
+     */
+    private $user_id;
+
+    /**
+     * @var \DateTime
+     */
+    private $expires;
+
+    /**
+     * @var string
+     */
+    private $scope;
+
+    /**
+     * @var \Eccube\Entity\OAuth2\Client
+     */
+    private $client;
+
+    /**
+     * @var \Eccube\Entity\OAuth2\User
+     */
+    private $user;
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set refresh_token
+     *
+     * @param string $refreshToken
+     * @return RefreshToken
+     */
+    public function setRefreshToken($refreshToken)
+    {
+        $this->refresh_token = $refreshToken;
+
+        return $this;
+    }
+
+    /**
+     * Get refresh_token
+     *
+     * @return string
+     */
+    public function getRefreshToken()
+    {
+        return $this->refresh_token;
+    }
+
+    /**
+     * Set client_id
+     *
+     * @param integer $clientId
+     * @return RefreshToken
+     */
+    public function setClientId($clientId)
+    {
+        $this->client_id = $clientId;
+
+        return $this;
+    }
+
+    /**
+     * Get client_id
+     *
+     * @return integer
+     */
+    public function getClientId()
+    {
+        return $this->client_id;
+    }
+
+    /**
+     * Set user_id
+     *
+     * @param integer $userId
+     * @return RefreshToken
+     */
+    public function setUserId($userId)
+    {
+        $this->user_id = $userId;
+
+        return $this;
+    }
+
+    /**
+     * Get user_id
+     *
+     * @return integer
+     */
+    public function getUserId()
+    {
+        return $this->user_id;
+    }
+
+    /**
+     * Set expires
+     *
+     * @param \DateTime $expires
+     * @return RefreshToken
+     */
+    public function setExpires($expires)
+    {
+        $this->expires = $expires;
+
+        return $this;
+    }
+
+    /**
+     * Get expires
+     *
+     * @return \DateTime
+     */
+    public function getExpires()
+    {
+        return $this->expires;
+    }
+
+    /**
+     * Set scope
+     *
+     * @param string $scope
+     * @return RefreshToken
+     */
+    public function setScope($scope)
+    {
+        $this->scope = $scope;
+
+        return $this;
+    }
+
+    /**
+     * Get scope
+     *
+     * @return string
+     */
+    public function getScope()
+    {
+        return $this->scope;
+    }
+
+    /**
+     * Set client
+     *
+     * @param \Eccube\Entity\OAuth2\Client $client
+     * @return RefreshToken
+     */
+    public function setClient(\Eccube\Entity\OAuth2\Client $client = null)
+    {
+        $this->client = $client;
+
+        return $this;
+    }
+
+    /**
+     * Get client
+     *
+     * @return \Eccube\Entity\OAuth2\Client
+     */
+    public function getClient()
+    {
+        return $this->client;
+    }
+
+    /**
+     * Set user
+     *
+     * @param \Eccube\Entity\OAuth2\User $user
+     * @return RefreshToken
+     */
+    public function setUser(\Eccube\Entity\OAuth2\User $user = null)
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    /**
+     * Get user
+     *
+     * @return \Eccube\Entity\OAuth2\User
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+}
